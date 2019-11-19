c76353c288f6d2ce86f62e8eba2ba1bee0853515
ShaneGuther <46198789+ShaneGuther@users.noreply.github.com>
2019-11-18 22:07:57 -0500

    Update Index
    Some comments/clean up

--- index.php
+++ index.php
@@ -5,34 +5,35 @@
 		This page will filter selection of music based on the parameters set by the user
 		-->
 		<meta charset="UTF-8">
-		<title>Album Database</title>
+		<title>Musical Storage</title>
 		<link rel="stylesheet" href="AlbumDB.css">
 	</head>
 	<body id="container">
+		<h1>Musical Database</h1>
 		<form method="POST" action="addition.php" id="forms">
-			<h1>Database</h1>
-			<br>
 			<input type="submit" name="submit" value="Add New Value" id="newBtn">
 		</form>
-
+		<br>
 		<form method="POST" action="#" id="form1">
 			<input type="text" name="tbd" id="deleteTf">
 			<input type="submit" name="Delete" value="Delete Value">
 		</form>
 
 		<form method="POST" action="#" id="form2">
-			<input type="checkbox" name="genres[]" value="HipHop" class="genChk"><label>Hip-Hop</label>
-			<input type="checkbox" name="genres[]" value="Rock" lass="genChk"><label>Rock</label>
+			<ul>
+			<li><input type="checkbox" name="genres[]" value="HipHop" class="genChk1"><label for="genres[1]">Hip-Hop</label></li>
+			<li><input type="checkbox" name="genres[]" value="Rock" class="genChk2"><label for="genres[2]">Rock</label></li>
 			<br>
-			<input type="checkbox" name="genres[]" value="SingerSongwriter" lass="genChk"><label>Singer/Songwriter</label>
-			<input type="checkbox" name="genres[]" value="Alternative" lass="genChk"><label>Alternative</label>
+			<li><input type="checkbox" name="genres[]" value="SingerSongwriter" class="genChk1"><label for="genres[3]">Singer/Songwriter</label></li>
+			<li><input type="checkbox" name="genres[]" value="Alternative" class="genChk2">	<label for="genres[4]">Alternative</label></li>
 			<br>
-			<input type="checkbox" name="genres[]" value="Jazz" lass="genChk"><label>Jazz</label>
-			<input type="checkbox" name="genres[]" value="Pop" lass="genChk"><label>Pop</label>
+			<li><input type="checkbox" name="genres[]" value="Jazz" class="genChk1"><label>Jazz</label for="genres[5]"></li>
+			<li><input type="checkbox" name="genres[]" value="Pop" class="genChk2"><label>Pop</label for="genres[6]"></li>
 			<br>
-			<input type="checkbox" name="genres[]" value="Country" lass="genChk"><label>Country</label>
-			<br>
+			<li><input type="checkbox" name="genres[]" value="Country" class="genChk2"><label for="genres[7]">Country</label></li>
+			</ul>
 			<input type="submit" name="submit" value="Update Table">
+
 		<?php
 		//Connect to the database using info in connect.php page
 		require("connect.php");
@@ -43,39 +44,37 @@
 		}catch(PDOException $e){
 		echo 'Connection error: ' . $e->getMessage();
 		}
-
-			// $hh = $_POST['Hiphop'];
-			// $rc = $_POST['Rock'];
-			// $ss = $_POST['SingerSongwriter'];
-			// $alt = $_POST['Alternative'];
-			// $jz = $_POST['Jazz'];
-			// $pop = $_POST['Pop'];
-			// $ct = $_POST['Country'];
-
 			$value = $_POST['value'];
 			$album = $_POST['album'];	
 			$artist = $_POST['artist'];
+
+			//Array of checkbox inputs for filtering
 			$genre = $_POST['genres'];
+			//to be delected - value that will be removed from table/database
 			$tbd = $_POST['tbd'];
 			
 			
+			//Takes the parameters from the array of checkboxes.  Can take 1 or many values, if none are selected full table is shown
 			$sqlGenres = " (";
 			foreach($genre as $index => $genres){
 					$sqlGenres .= "'".$genres."'".",";
-				// }elseif($sqlGenres != ""){
-				// }
 				}
 			
 			if($sqlGenres != " ("){
 			$sqlGenres = rtrim($sqlGenres, ",");
 			$sqlGenres .= ")";
 		}
+
+
 		//Will delete data from the table according to entry into the text field, if nothing is entered query will not execute
 		if(isset($tbd)){
 		$command = "DELETE FROM album WHERE Value='$tbd'";
 			$deletion = $dbConn->prepare($command);
 			$execOk = $deletion->execute();
 		}
+
+
+
 		//Will add a new entry to the database when executed from addition.php -- only runs if value is set
 		if(isset($value)){
 			try{
@@ -89,19 +88,19 @@
 
 		}
 
+		//if any checkboxes are selected, this will select only the selected genres
 		if($sqlGenres != " ("){
 			$comm = "SELECT Value, Album, Artist, Genre FROM album WHERE Genre IN" . $sqlGenres;
 			$stmnt = $dbConn->query($comm);
 			$execOk = $stmnt->execute();
-			//if($execOk){
 				echo '<table>';
 				echo '<tr><td>Value</td><td>Album</td><td>Artist</td><td>Genre</td></tr>';
 			while($row = $stmnt->fetch()){
 				echo '<tr><td>' . $row[Value] . '</td><td>' . $row[Album] . '</td><td>' . $row[Artist] . '</td><td>' . $row[Genre] . '</td></tr>';
 				}
 				echo '</table>';
-			//}
-
+			
+		//If no checkboxes are selected, select all entries in database
 		}else{
 		$command = "SELECT Value, Album, Artist, Genre FROM album ORDER BY Value";
 		$statement = $dbConn->query($command);
@@ -116,20 +115,8 @@
 		}
 		}
 		
-		//$command = "SELECT Value, Album, Artist, Genre FROM album ORDER BY Value";
-		//$statement = $dbConn->query($command);
-		//$execOk = $statement->execute();
-
-		
-		// if($execOk){
-		// 	echo '<table>';
-		// 	echo '<tr><td>Value</td><td>Album</td><td>Artist</td><td>Genre</td></tr>';
-		// 	while($row = $statement->fetch()){
-		// 		echo '<tr><td>' . $row[Value] . '</td><td>' . $row[Album] . '</td><td>' . $row[Artist] . '</td><td>' . $row[Genre] . '</td></tr>';
-		// 	}
-		// 	echo '</table>';
-		// }
 				?>
 		</form>
 	</body>
 </html>
+
